71fca9a41a9564e57b9146272bbf940eb691803b
LimberLumberSlumber <limberlumberslumber@gmail.com>
2021-10-04 18:47:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -0,0 +1,25 @@
+package com.example.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        //ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("kGkNFIk8fr1mEJJxxg1ebBJAdvMohv4ZNUDLkLvY")
+                .clientKey("D4iBkrCIm9iYhoIxcvHrxy279pLtqRbgqUYE3OfT")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
